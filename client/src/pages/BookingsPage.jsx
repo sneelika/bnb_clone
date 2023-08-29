bd4ba56e7f343feb638eeba44a6b0d8c2dbbee3b
sneelika <sayuruneelika@gmail.com>
2023-08-29 10:52:42 +0530

    bookingpages.js

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -2,7 +2,6 @@ import AccountNav from "./../components/AccountNav";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import PlaceImg from "./../components/PlaceImg";
-import { differenceInCalendarDays, format } from "date-fns";
 import { Link } from "react-router-dom";
 import BookingDates from "./../components/BookingDates";
 
